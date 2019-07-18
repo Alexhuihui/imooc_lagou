88b2aea3be309807e65efac084cd67de5755e3ce
wangyonghui <2930807240@qq.com>
2019-07-18 23:42:09 +0800

    考虑怎么把自己的爬虫代理池在代码中使用

--- handle_crawl_lagou.py
+++ handle_crawl_lagou.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
 import re
+import time
+import json
+import multiprocessing
 
 __author__ = 'alex'
 
@@ -20,14 +23,58 @@ class HandleLaGou:
 		city_url = 'https://www.lagou.com/jobs/allCity.html'
 		city_result = self.handle_request(method='GET', url=city_url)
 		self.city_list = city_search.findall(city_result)
-		print(self.city_list)
+		self.lagou_session.cookies.clear()
+		
+	def handle_city_job(self, city):
+		first_request_url = "https://www.lagou.com/jobs/list_python?&px=default&city=%s" % city
+		first_response = self.handle_request(method='GET', url=first_request_url)
+		total_page_search = re.compile(r'class="span\stotalNum">(\d+)</span>')
+		try:
+			total_page = total_page_search.search(first_response).group(1)
+		except:
+			return
+		for i in range(1, int(total_page) + 1):
+			data = {
+				"pn": i,
+				"kd": "python"
+			}
+			page_url = "https://www.lagou.com/jobs/positionAjax.json?px=default&city=%s&needAddtionalResult=false" % city
+			referer_url = "https://www.lagou.com/jobs/list_python?px=default&city=%s" % city
+			self.header['Referer'] = referer_url.encode()
+			response = self.handle_request(url=page_url, method='POST', data=data, info=city)
+			
+			lagou_data = json.loads(response)
+			job_list = lagou_data['content']['positionResult']['result']
+			for job in job_list:
+				print(job)
 	
 	def handle_request(self, method, url, data=None, info=None):
-		if method == "GET":
-			response = self.lagou_session.get(url=url, headers=self.header)
-		return response.text
+		while True:
+			if method == "GET":
+				response = self.lagou_session.get(url=url, headers=self.header)
+			elif method == "POST":
+				response = self.lagou_session.post(url=url, headers=self.header, data=data)
+			response.encoding = 'utf-8'
+			if '频繁' in response.text:
+				self.lagou_session.cookies.clear()
+				first_request_url = "https://www.lagou.com/jobs/list_python?&px=default&city=%s" % info
+				self.handle_request(method='GET', url=first_request_url)
+				time.sleep(10)
+				continue
+			return response.text
+	
+	def get_proxy(self):
+		return requests.get("http://120.79.226.196:5010/get/").content
+	
+	def delete_proxy(self, proxy):
+		requests.get("http://120.79.226.196:5010/delete/?proxy={}".format(proxy))
 
 
 if __name__ == '__main__':
 	lagou = HandleLaGou()
 	lagou.handle_city()
+	pool = multiprocessing.Pool(2)
+	for city in lagou.city_list:
+		pool.apply_async(lagou.handle_city_job, args=(city,))
+	pool.close()
+	pool.join()
